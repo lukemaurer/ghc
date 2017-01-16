b04f8b3d4cc3822542f5c6a990ae81c6b4401e15
Luke Maurer <maurerl@cs.uoregon.edu>
2017-01-15 17:36:32 -0800

    Use isReflexiveCo

--- compiler/coreSyn/CoreArity.hs
+++ compiler/coreSyn/CoreArity.hs
@@ -911,12 +911,10 @@ instance Outputable EtaInfo where
 
 pushCoercion :: Coercion -> [EtaInfo] -> [EtaInfo]
 pushCoercion co1 (EtaCo co2 : eis)
-  | redundant   = eis
-  | otherwise   = EtaCo co : eis
+  | isReflexiveCo co = eis
+  | otherwise        = EtaCo co : eis
   where
     co = co1 `mkTransCo` co2
-    redundant = isReflCo co || from_ty `eqType` to_ty
-    Pair from_ty to_ty = coercionKind co
 
 pushCoercion co eis = EtaCo co : eis
 
